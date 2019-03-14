b9ffa81bcc7b9ff894e1b374a42521e9f5f7a489
Abby Gottlich <abby.gottlich@gmail.com>
2019-03-14 17:50:18 -0500

    home page basic layout and styling done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div className="main">
+          <div className="header">
+            <div className="logo"></div>
+            <div class="nav">
+              <div className="nav-text">Home</div>
+              <div className="nav-text">About</div>
+              <div className="nav-text">Contact</div>
+            </div>
+          </div>
+          <div className="development">
+            <div className="development-text">&lt;Development /&gt;</div>
+          </div>
+          <div className="design">
+            <div className="design-text"></div>
+          </div>
+        </div>
       </div>
     );
   }
