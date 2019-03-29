1750da3053a71bfa1978d660d8980aa8e045c541
Abby Gottlich <abby.gottlich@gmail.com>
2019-03-28 20:50:49 -0500

    created development route

--- src/App.js
+++ src/App.js
@@ -1,27 +1,32 @@
 import React, { Component } from 'react';
 import './App.css';
+import { BrowserRouter, Route } from "react-router-dom";
+import Development from "./components/Development"
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="main">
-          <div className="header">
-            <div className="logo"></div>
-            <div class="nav">
-              <div className="nav-text">Home</div>
-              <div className="nav-text">About</div>
-              <div className="nav-text">Contact</div>
+      <BrowserRouter>
+        <div className="App">
+          <div className="main">
+            <div className="header">
+              <div className="logo"></div>
+              <div class="nav">
+                <div className="nav-text">Home</div>
+                <div className="nav-text">About</div>
+                <div className="nav-text">Contact</div>
+              </div>
+            </div>
+            <div className="development">
+              <div className="development-text">&lt;Development /&gt;</div>
+              <Route path="/development" component={Development} />
+            </div>
+            <div className="design">
+              <div className="design-text"></div>
             </div>
           </div>
-          <div className="development">
-            <div className="development-text">&lt;Development /&gt;</div>
-          </div>
-          <div className="design">
-            <div className="design-text"></div>
-          </div>
         </div>
-      </div>
+      </BrowserRouter>
     );
   }
 }
---
+++ src/components/Development.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Development extends Component {
+    render() {
+        return (
+            <div>Hello</div>
+        );
+    }
+}
+
+export default Development;
