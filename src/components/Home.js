5c635dfbc5ab8b052d0e1f35a2abdb2b7b57a5c5
Abby Gottlich <abby.gottlich@gmail.com>
2019-04-20 09:52:23 -0500

    development toggle working

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,41 @@
 import React, { Component } from 'react';
 
 class About extends Component {
+
+    state = {
+        toggle: false
+    }
+    toggleHandler() {
+        if (this.state.toggle === false) {
+            this.setState({
+                toggle: true
+            })
+        } else {
+            this.setState({
+                toggle: false
+            })
+        }
+    }
+    showDevelopmentLinks() {
+        if (this.state.toggle === true) {
+            return (
+                <div>
+                    <div>link 1</div>
+                    <div>link 2</div>
+                    <div>link 3</div>
+                </div>
+            )
+        } else {
+            return null
+        }
+    }
+
     render() {
         return (
             <div className="App">
                 <div className="main">
                     <div className="development">
-                        <div className="development-text">&lt;Development /&gt;</div>
+                        <div className="development-text" onClick={() => { this.toggleHandler() }} >&lt;Development /&gt;  {this.showDevelopmentLinks()}</div>
                     </div>
                     <div className="design">
                         <div className="design-text"></div>
