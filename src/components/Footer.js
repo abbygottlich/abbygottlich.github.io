1430eeeab9829e3734fa4357be008322681b6514
Abby Gottlich <abby.gottlich@gmail.com>
2019-05-01 19:45:53 -0500

    social icons styled and linked

---
+++ src/components/Footer.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class Footer extends Component {
+    render() {
+        return (
+            <div className="social-icons">
+                <a href="https://www.linkedin.com/in/abbygottlich" target="blank">
+                    <div className="linkedin-icon"></div>
+                </a>
+                <a href="https://github.com/abbygottlich" target="blank">
+                    <div className="github-icon"></div>
+                </a>
+            </div>
+        );
+    }
+}
+
+export default Footer;
