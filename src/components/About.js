8a87aeb50a753a211f441015f464fa91e42e9e79
Abby Gottlich <abby.gottlich@gmail.com>
2019-05-02 18:59:26 -0500

    about page desktop styling done

--- src/components/About.js
+++ src/components/About.js
@@ -3,7 +3,12 @@ import React, { Component } from 'react';
 class About extends Component {
     render() {
         return (
-            <div className="about">Welcome to the About Page!</div>
+            <div className="about">
+                <div className="about-content">
+                    <div className="intro">Hi, I'm Abby. I am a Developer-Designer Hybrid seeking a job that allows me to do both.</div>
+                    <div className="about-paragraph">I am a full-stack web developer who likes to concentrate on the Front-End. With a background in Visual/UX Design, I bring something to the table that most developers do not...an eye for design! I enjoy cooking, snuggling with my dogs, and being employed. Please take a look at my <a href="./">resume</a> for more information about me!</div>
+                </div>
+            </div>
         );
     }
 }
