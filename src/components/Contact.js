428677457329a1b7af65f26e42eb6402bd31af8e
Abby Gottlich <abby.gottlich@gmail.com>
2019-05-01 21:30:17 -0500

    began styling for about and contect pages and imported CSSTransitions

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class Contact extends Component {
     render() {
         return (
-            <div>Welcome to the Contact Page!</div>
+            <div className="contact">Welcome to the Contact Page!</div>
         );
     }
 }
