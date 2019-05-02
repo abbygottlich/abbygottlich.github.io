1af03015374396041ec2deb1ee7747ce74c61a0b
Abby Gottlich <abby.gottlich@gmail.com>
2019-05-01 23:31:43 -0500

    menu started

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,15 +2,57 @@ import React, { Component } from 'react';
 import { Link } from "react-router-dom";
 
 class Header extends Component {
+
+    state = {
+        menuIcon: "menu",
+        toggle: false
+    }
+
+    toggleHandler() {
+        if (this.state.toggle === false) {
+            this.setState({
+                toggle: true,
+                menuIcon: "x"
+            })
+        } else {
+            this.setState({
+                toggle: false,
+                menuIcon: "menu"
+            })
+        }
+    }
+
+    showMenu() {
+        if (this.state.toggle === true) {
+            return (
+                <div className="menu-block">
+                    <Link to="/" className="menu-home">
+                        <div className="menu-text">Home</div>
+                    </Link>
+                    <Link to="/about" className="menu-about">
+                        <div className="menu-text">About</div>
+                    </Link>
+                    <Link to="/contact" className="menu-contact">
+                        <div className="menu-text">Contact</div>
+                    </Link>
+                </div>
+            )
+        } else {
+            return null
+        }
+    }
+
     render() {
         return (
             <div className="header">
                 <Link to="/" className="logo"></Link>
-                <div className="nav">
+                {/* <div className="nav">
                     <Link to="/" className="nav-text">Home</Link>
                     <Link to="/about" className="nav-text">About</Link>
                     <Link to="/contact" className="nav-text">Contact</Link>
-                </div>
+                </div> */}
+                <button className={this.state.menuIcon} onClick={this.toggleHandler}></button>
+                <div>{this.showMenu()}</div>
             </div>
         );
     }
