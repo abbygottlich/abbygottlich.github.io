9c2062b8e2f5262ec046d37a068edd62b160fd9f
Abby Gottlich <abby.gottlich@gmail.com>
2019-05-02 21:27:40 -0500

    added resume link

--- src/components/About.js
+++ src/components/About.js
@@ -6,7 +6,7 @@ class About extends Component {
             <div className="about">
                 <div className="about-content">
                     <div className="intro">Hi, I'm Abby. I am a Developer-Designer Hybrid seeking a job that allows me to do both.</div>
-                    <div className="about-paragraph">I am a full-stack web developer who likes to concentrate on the Front-End. With a background in Visual/UX Design, I bring something to the table that most developers do not...an eye for design! I enjoy cooking, snuggling with my dogs, and being employed. Please take a look at my <a href="./">resume</a> for more information about me!</div>
+                    <div className="about-paragraph">I am a full-stack web developer who likes to concentrate on the Front-End. With a background in Visual/UX Design, I bring something to the table that most developers do not...an eye for design! I enjoy cooking, snuggling with my dogs, and being employed. Please take a look at my <a className="resume" href="https://github.com/abbygottlich/resume/blob/master/abby-gottlich-resume.pdf" target="blank">resume</a> for more information about me!</div>
                 </div>
             </div>
         );
