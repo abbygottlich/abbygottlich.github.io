a8644e66735d537cc6f77d88e65c1e80d52758a0
Abby Gottlich <abby.gottlich@gmail.com>
2019-05-06 17:29:19 -0500

    fixed design modal glitch by moving all modal content into design file

--- src/components/Design.js
+++ src/components/Design.js
@@ -1,8 +1,4 @@
 import React, { Component } from 'react';
-import NakedTravelModal from "./NakedTravelModal";
-import AviateModal from "./AviateModal";
-import ElmCreekModal from "./ElmCreekModal";
-import AlexModal from "./AlexModal";
 
 class Design extends Component {
 
@@ -41,7 +37,48 @@ class Design extends Component {
 
     showNakedTravelModal() {
         if (this.state.nakedTravelModalToggle === true) {
-            return <NakedTravelModal />
+            return <div className="modal-content">
+                <div className="modal-header">
+                    <div className="project-name">NakedTravel UX Design</div>
+                    <a className="prototype" href="https://projects.invisionapp.com/share/P9E50REUB#/screens" target="blank">View Prototype</a>
+                    <div onClick={() => { this.nakedTravelModalToggle() }} className="x"></div>
+                </div>
+                <div className="project-images">
+                    <img className="project-image" alt="project" src={require('../naked-travel/slide-1.jpg')} />
+                    <img className="project-image" alt="project" src={require('../naked-travel/slide-2.jpg')} />
+                    <img className="project-image" alt="project" src={require('../naked-travel/slide-3.jpg')} />
+                    <img className="project-image" alt="project" src={require('../naked-travel/slide-3.5.jpg')} />
+                    <img className="project-image" alt="project" src={require('../naked-travel/slide-4.jpg')} />
+                    <img className="project-image" alt="project" src={require('../naked-travel/slide-5.jpg')} />
+                    <img className="project-image" alt="project" src={require('../naked-travel/slide-6.jpg')} />
+                    <img className="project-image" alt="project" src={require('../naked-travel/slide-7.jpg')} />
+                    <img className="project-image" alt="project" src={require('../naked-travel/slide-8.jpg')} />
+                    <img className="project-image" alt="project" src={require('../naked-travel/slide-9.jpg')} />
+                    <img className="project-image" alt="project" src={require('../naked-travel/slide-10.jpg')} />
+                    <img className="project-image" alt="project" src={require('../naked-travel/slide-11.jpg')} />
+                    <img className="project-image" alt="project" src={require('../naked-travel/slide-12.jpg')} />
+                    <img className="project-image" alt="project" src={require('../naked-travel/slide-13.jpg')} />
+                    <img className="project-image" alt="project" src={require('../naked-travel/slide-14.jpg')} />
+                    <img className="project-image" alt="project" src={require('../naked-travel/slide-15.jpg')} />
+                    <img className="project-image" alt="project" src={require('../naked-travel/slide-16.jpg')} />
+                    <img className="project-image" alt="project" src={require('../naked-travel/slide-17.jpg')} />
+                    <img className="project-image" alt="project" src={require('../naked-travel/slide-18.jpg')} />
+                    <img className="project-image" alt="project" src={require('../naked-travel/slide-19.jpg')} />
+                    <img className="project-image" alt="project" src={require('../naked-travel/slide-20.jpg')} />
+                    <img className="project-image" alt="project" src={require('../naked-travel/slide-21.jpg')} />
+                    <img className="project-image" alt="project" src={require('../naked-travel/slide-22.jpg')} />
+                    <img className="project-image" alt="project" src={require('../naked-travel/slide-23.jpg')} />
+                    <img className="project-image" alt="project" src={require('../naked-travel/slide-24.jpg')} />
+                    <img className="project-image" alt="project" src={require('../naked-travel/slide-25.jpg')} />
+                    <img className="project-image" alt="project" src={require('../naked-travel/slide-26.jpg')} />
+                    <img className="project-image" alt="project" src={require('../naked-travel/slide-28.jpg')} />
+                    <img className="project-image" alt="project" src={require('../naked-travel/slide-29.jpg')} />
+                    <img className="project-image" alt="project" src={require('../naked-travel/slide-30.jpg')} />
+                    <img className="project-image" alt="project" src={require('../naked-travel/slide-31.jpg')} />
+                    <img className="project-image" alt="project" src={require('../naked-travel/slide-32.jpg')} />
+                    <img className="project-image" alt="project" src={require('../naked-travel/brand-identity.jpg')} />
+                </div>
+            </div>
         } else return null
     }
 
@@ -60,7 +97,29 @@ class Design extends Component {
 
     showAviateModal() {
         if (this.state.aviateModalToggle === true) {
-            return <AviateModal />
+            return <div className="modal-content">
+                <div className="modal-header">
+                    <div className="project-name">Aviate App + Designs</div>
+                    <a className="prototype" href="https://marvelapp.com/14fg916/screen/10797059" target="blank">View Prototype</a>
+                    <div onClick={() => { this.aviateModalToggle() }} className="x"></div>
+                </div>
+                <div className="project-images">
+                    <img className="project-image" alt="project" src={require('../aviate/aviate-slide-1.jpg')} />
+                    <img className="project-image" alt="project" src={require('../aviate/aviate-slide-2.jpg')} />
+                    <img className="project-image" alt="project" src={require('../aviate/aviate-slide-3.jpg')} />
+                    <img className="project-image" alt="project" src={require('../aviate/aviate-slide-4.jpg')} />
+                    <img className="project-image" alt="project" src={require('../aviate/aviate-slide-5.jpg')} />
+                    <img className="project-image" alt="project" src={require('../aviate/aviate-slide-6.jpg')} />
+                    <img className="project-image" alt="project" src={require('../aviate/aviate-slide-7.jpg')} />
+                    <img className="project-image" alt="project" src={require('../aviate/aviate-slide-8.jpg')} />
+                    <img className="project-image" alt="project" src={require('../aviate/aviate-slide-9.jpg')} />
+                    <img className="project-image" alt="project" src={require('../aviate/aviate-slide-10.jpg')} />
+                    <img className="project-image" alt="project" src={require('../aviate/aviate-slide-11.jpg')} />
+                    <img className="project-image" alt="project" src={require('../aviate/aviate-slide-12.jpg')} />
+                    <img className="project-image" alt="project" src={require('../aviate/aviate-slide-13.jpg')} />
+                    <img className="project-image" alt="project" src={require('../aviate/brand-guidelines.jpg')} />
+                </div>
+            </div>
         } else return null
     }
 
@@ -79,7 +138,21 @@ class Design extends Component {
 
     showElmCreekModal() {
         if (this.state.elmCreekToggle === true) {
-            return <ElmCreekModal />
+            return <div className="modal-content">
+                <div className="modal-header">
+                    <div className="project-name">Elm Creek Website + Branding</div>
+                    <a className="prototype" href="https://marvelapp.com/133fji06/screen/37485606" target="blank">View Prototype</a>
+                    <div onClick={() => { this.elmCreekModalToggle() }} className="x"></div>
+                </div>
+                <div className="project-images">
+                    <img className="project-image" alt="project" src={require('../elm-creek/elm-creek-slide-1.jpg')} />
+                    <img className="project-image" alt="project" src={require('../elm-creek/elm-creek-slide-2.jpg')} />
+                    <img className="project-image" alt="project" src={require('../elm-creek/elm-creek-slide-3.jpg')} />
+                    <img className="project-image" alt="project" src={require('../elm-creek/elm-creek-slide-4.jpg')} />
+                    <img className="project-image" alt="project" src={require('../elm-creek/elm-creek-slide-5.jpg')} />
+                    <img className="project-image" alt="project" src={require('../elm-creek/brand-guidelines.jpg')} />
+                </div>
+            </div>
         } else return null
     }
 
@@ -98,7 +171,21 @@ class Design extends Component {
 
     showAlexModal() {
         if (this.state.alexModalToggle === true) {
-            return <AlexModal />
+            return <div className="modal-content">
+                <div className="modal-header">
+                    <div className="project-name">Alex City Council</div>
+                    <div className="project-name">Campaign Website</div>
+                    <div onClick={() => { this.alexModalToggle() }} className="x"></div>
+                </div>
+                <div className="project-images">
+                    <img className="project-image" alt="project" src={require('../alex-campaign/alex-slide-1.jpg')} />
+                    <img className="project-image" alt="project" src={require('../alex-campaign/alex-slide-2.jpg')} />
+                    <img className="project-image" alt="project" src={require('../alex-campaign/alex-slide-3.jpg')} />
+                    <img className="project-image" alt="project" src={require('../alex-campaign/alex-slide-4.jpg')} />
+                    <img className="project-image" alt="project" src={require('../alex-campaign/alex-slide-5.jpg')} />
+                    <img className="project-image" alt="project" src={require('../alex-campaign/alex-slide-6.jpg')} />
+                </div>
+            </div>
         } else return null
     }
 
