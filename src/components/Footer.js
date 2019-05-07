97202432854333c2f63128218fac3640fbd6eb1e
Abby Gottlich <abby.gottlich@gmail.com>
2019-05-07 16:10:38 -0500

    footer positioned fixed to stay put on scroll on tablet and mobile

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,13 +3,15 @@ import React, { Component } from 'react';
 class Footer extends Component {
     render() {
         return (
-            <div className="social-icons">
-                <a href="https://www.linkedin.com/in/abbygottlich" target="blank">
-                    <div className="linkedin-icon"></div>
-                </a>
-                <a href="https://github.com/abbygottlich" target="blank">
-                    <div className="github-icon"></div>
-                </a>
+            <div className="footer">
+                <div className="social-icons">
+                    <a href="https://www.linkedin.com/in/abbygottlich" target="blank">
+                        <div className="linkedin-icon"></div>
+                    </a>
+                    <a href="https://github.com/abbygottlich" target="blank">
+                        <div className="github-icon"></div>
+                    </a>
+                </div>
             </div>
         );
     }
